bdd43445ac098c9f3f42d51b1bbc58127f6d9b31
Camillox2 <vitorcamilloh@gmail.com>
2024-11-06 19:07:15 -0300

    entrega

---
+++ DeD/app/src/main/java/com/example/aplicacao_ofc/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.aplicacao_ofc
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        val playButton = findViewById<Button>(R.id.button_play)
+        val config = findViewById<Button>(R.id.button_config)
+
+        playButton.setOnClickListener{
+            val intent = Intent(this, CharacterCreation::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
+
+
---
+++ DeD/app/src/main/java/com/example/aplicacao_ofc/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.aplicacao_ofc.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF240079)
+val White = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Black = Color(0xFF0E0606)
+val PurpleGrey40 = Color(0xFFF5F5F5)
+val Pink40 = Color(0xFFFFFCFC)
